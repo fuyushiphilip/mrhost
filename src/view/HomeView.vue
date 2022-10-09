b22aaef158ecd5e0584ba43ad6417e2686a8e266
FYS <fysphilip@gmail.com>
2022-10-09 14:57:11 +0800

    filter section
    filter function finished

--- src/view/HomeView.vue
+++ src/view/HomeView.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="d-flex col-md-auto">
       <!-- filter section -->
-      <div class="w-25 row-md-auto">
+      <div class="w-25 row-md-auto mt-3 filter-section">
         <div class="">
           <h5>透過以下分類搜尋</h5>
         </div>
@@ -28,10 +28,11 @@
       <!-- Hotel Card section -->
       <div class="container">
         <div class="d-flex justify-content-between py-3">
-          <h3 class="col font-size px-2">找到{{ dataLen }}間住宿</h3>
+          <h3 class="col font-size px-2">找到{{ hotelsData.length }}間住宿</h3>
         </div>
-        <div class="col" v-for="hotel in data" :key="hotel.id">
-          <Hotel v-bind:hotel="hotel" v-bind:dataLen="dataLen" />
+
+        <div class="col" v-for="hotel in hotelsData" :key="hotel.id">
+          <Hotel v-bind:hotel="hotel" />
         </div>
       </div>
     </div>
@@ -40,24 +41,42 @@
 
 <script>
 import HotelCard from "../components/HotelCard.vue";
-import jsonData from "../data/hotels.json";
+import hotels from "../data/hotels.js";
+
 export default {
   components: {
     Hotel: HotelCard,
   },
-  data() {
-    return {
-      data: jsonData.hotels,
-      dataLen: Object.keys(jsonData.hotels).length,
-      facilities: [
-        "禁菸客房",
-        "廚房",
-        "洗衣房",
-        "停車場",
-        "寵物友善",
-        "免費無線網路",
-      ],
-    };
+  data: () => ({
+    hotelsData: hotels,
+    facilities: [
+      "禁菸客房",
+      "廚房",
+      "洗衣房",
+      "停車場",
+      "寵物友善",
+      "免費無線網路",
+    ],
+    checkedFacilities: [],
+    selectedSorting: "0",
+  }),
+  methods: {
+    filterHotels() {
+      console.log("filter");
+
+      var tmpHotels = [];
+      for (var i = 0; i < hotels.length; i++) {
+        for (var j = 0; j < hotels[i].facilities.length; j++) {
+          if (this.checkedFacilities.includes(hotels[i].facilities[j])) {
+            tmpHotels.push(hotels[i]);
+            console.log("have");
+            break;
+          }
+        }
+      }
+      if (tmpHotels.length == 0) tmpHotels = hotels;
+      this.hotelsData = tmpHotels;
+    },
   },
 };
 </script>
@@ -65,5 +84,12 @@ export default {
 <style scoped>
 .font-size {
   font-size: 24px;
+}
+
+.filter-section {
+  border: 1px solid;
+  border-color: #d7dce3;
+  height: 50%;
+  padding: 20px;
 }
 </style>
